2e2585e3cebcd41716cf49a905975b611968d80a
sourabh-swargam <swargam.sourabh@gmail.com>
2020-04-13 17:47:21 +0530

    lru template created

---
+++ LRU/lru.py
@@ -0,0 +1,14 @@
+class LRU:
+
+	def __init__(self, capacity):
+		self.capacity = capacity
+		self.cache = []
+
+	def put(self, item):
+		return
+
+	def get(self):
+		return
+
+	def get_cache(self):
+		return
